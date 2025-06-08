f43239fb1cf5d9a5304ef55976a07c85c57d6f55
Your Name <you@example.com>
2025-06-08 18:37:28 +0900

    サーバーとの通信部分を修正。

--- server/server.js
+++ server/server.js
@@ -27,23 +27,25 @@ const clients = new Set();
 
 // WebSocket接続の処理
 wss.on('connection', (ws) => {
+    console.log('接続');
     clients.add(ws);
 
     // メッセージ受信時の処理
     ws.on('message', (message) => {
+        console.log(message);
         let jsonMessage;
         try {
             jsonMessage = JSON.parse(message.toString());
             
             // IPアドレスを追加
             const ip = ws._socket.remoteAddress;
-            jsonMessage.fromIP = ip;
+            jsonMessage.from = ip;
 
             // toIPが指定されている場合は特定のクライアントのみに送信
             // 指定がない場合は全クライアントにブロードキャスト
             wss.clients.forEach((client) => {
                 if (client !== ws && client.readyState === WebSocket.OPEN) {
-                    if (!jsonMessage.toIP || client._socket.remoteAddress === jsonMessage.toIP) {
+                    if (!jsonMessage.to || client._socket.remoteAddress === jsonMessage.to) {
                         client.send(JSON.stringify(jsonMessage));
                     }
                 }
